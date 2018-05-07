373fb7f0253b0f70798f39905e34f45e03b42b8c
Suchitra Naik <susanaik@gmail.com>
2018-05-07 13:37:16 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,8 @@ class Project
   
   def add_backer(backer_instance)
     @backers << backer_instance
-    #backer_instance.back_project(self) if backer_instance.back_project != self
+    backer_instance.back_project(self) 
+    #if backer_instance.back_project != self
   end 
   
 end 
