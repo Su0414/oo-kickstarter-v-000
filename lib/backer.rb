ded8acb84c47ddd71bf7fb7d6f323c5dbaa6957e
Suchitra Naik <susanaik@gmail.com>
2018-05-07 13:18:23 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,20 +12,7 @@ class Backer
     @name = name 
   end
   
-  def backed_projects=(backed_projects)
-    if(self.@backed_projects.empty?)
-      self.@backed_projects = Project.new(backed_projects)
-    end 
-  end 
   
-  def project_name=(project_name)
-    if(self.backed_projects.nil?)
-      self.backed_projects = Project.find_or_create_by_name(project_name)
-      
-    else 
-      self.backed_projects.name = project_name
-    end 
-  end 
  
   def back_project(project) 
     @backed_projects << project
