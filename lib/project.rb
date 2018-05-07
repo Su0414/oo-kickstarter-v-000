09d39614eca12f896a1b25a6c9367f8eafbac043
Suchitra Naik <susanaik@gmail.com>
2018-05-07 13:17:28 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,21 +7,6 @@ class Project
   end 
   
   
-   def self.create_by_title(project_name)
-    new_project = self.new(project_name)
-    new_project.save 
-    new_project.title = project_name
-    new_project
-  end 
-  
-  
-  def self.find_by_name(new_project)
-    @@all.detect{|new_projects| new_projects.title == new_project}
-  end 
-  
-  def self.find_or_create_by_name(project_name)
-    self.find_by_name(project_name) || self.create_by_name(project_name)
-  end 
   
   
   def add_backer(backer)
